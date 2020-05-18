4956b9931810b3ce86452006dd342cda2ff96bac
Harun Mbaabu Mwenda <mbaabuharun8@gmail.com>
2020-05-18 14:50:08 +0300

    first commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,287 @@
+import { Component } from '@angular/core';
+import { Resume, Experience, Education, Skill } from './resume';
+import { ScriptService } from './script.service';
+declare let pdfMake: any ;
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+
+  resume = new Resume();
+
+  degrees = ['B.E.', 'M.E.', 'B.Com', 'M.Com'];
+
+  constructor(private scriptService: ScriptService) {
+    this.resume = JSON.parse(sessionStorage.getItem('resume')) || new Resume();
+    if (!this.resume.experiences || this.resume.experiences.length === 0) {
+      this.resume.experiences = [];
+      this.resume.experiences.push(new Experience());
+    }
+    if (!this.resume.educations || this.resume.educations.length === 0) {
+      this.resume.educations = [];
+      this.resume.educations.push(new Education());
+    }
+    if (!this.resume.skills || this.resume.skills.length === 0) {
+      this.resume.skills = [];
+      this.resume.skills.push(new Skill());
+    }
+
+    console.log('Loading External Scripts');
+    this.scriptService.load('pdfMake', 'vfsFonts');
+  }
+
+  addExperience() {
+    this.resume.experiences.push(new Experience());
+  }
+
+  addEducation() {
+    this.resume.educations.push(new Education());
+  }
+
+  generatePdf(action = 'open') {
+    console.log(pdfMake);
+    const documentDefinition = this.getDocumentDefinition();
+
+    switch (action) {
+      case 'open': pdfMake.createPdf(documentDefinition).open(); break;
+      case 'print': pdfMake.createPdf(documentDefinition).print(); break;
+      case 'download': pdfMake.createPdf(documentDefinition).download(); break;
+
+      default: pdfMake.createPdf(documentDefinition).open(); break;
+    }
+
+  }
+
+
+  resetForm() {
+    this.resume = new Resume();
+  }
+
+  getDocumentDefinition() {
+    sessionStorage.setItem('resume', JSON.stringify(this.resume));
+    return {
+      content: [
+        {
+          text: 'RESUME',
+          bold: true,
+          fontSize: 20,
+          alignment: 'center',
+          margin: [0, 0, 0, 20]
+        },
+        {
+          columns: [
+            [{
+              text: this.resume.name,
+              style: 'name'
+            },
+            {
+              text: this.resume.address
+            },
+            {
+              text: 'Email : ' + this.resume.email,
+            },
+            {
+              text: 'Contant No : ' + this.resume.contactNo,
+            },
+            {
+              text: 'GitHub: ' + this.resume.socialProfile,
+              link: this.resume.socialProfile,
+              color: 'blue',
+            }
+            ],
+            [
+              this.getProfilePicObject()
+            ]
+          ]
+        },
+        {
+          text: 'Skills',
+          style: 'header'
+        },
+        {
+          columns : [
+            {
+              ul : [
+                ...this.resume.skills.filter((value, index) => index % 3 === 0).map(s => s.value)
+              ]
+            },
+            {
+              ul : [
+                ...this.resume.skills.filter((value, index) => index % 3 === 1).map(s => s.value)
+              ]
+            },
+            {
+              ul : [
+                ...this.resume.skills.filter((value, index) => index % 3 === 2).map(s => s.value)
+              ]
+            }
+          ]
+        },
+        {
+          text: 'Experience',
+          style: 'header'
+        },
+        this.getExperienceObject(this.resume.experiences),
+
+        {
+          text: 'Education',
+          style: 'header'
+        },
+        this.getEducationObject(this.resume.educations),
+        {
+          text: 'Other Details',
+          style: 'header'
+        },
+        {
+          text: this.resume.otherDetails
+        },
+        {
+          text: 'Signature',
+          style: 'sign'
+        },
+        {
+          columns : [
+              { qr: this.resume.name + ', Contact No : ' + this.resume.contactNo, fit : 100 },
+              {
+              text: `(${this.resume.name})`,
+              alignment: 'right',
+              }
+          ]
+        }
+      ],
+      info: {
+        title: this.resume.name + '_RESUME',
+        author: this.resume.name,
+        subject: 'RESUME',
+        keywords: 'RESUME, ONLINE RESUME',
+      },
+        styles: {
+          header: {
+            fontSize: 18,
+            bold: true,
+            margin: [0, 20, 0, 10],
+            decoration: 'underline'
+          },
+          name: {
+            fontSize: 16,
+            bold: true
+          },
+          jobTitle: {
+            fontSize: 14,
+            bold: true,
+            italics: true
+          },
+          sign: {
+            margin: [0, 50, 0, 10],
+            alignment: 'right',
+            italics: true
+          },
+          tableHeader: {
+            bold: true,
+          }
+        }
+    };
+  }
+
+  getExperienceObject(experiences: Experience[]) {
+
+    const exs = [];
+
+    experiences.forEach(experience => {
+      exs.push(
+        [{
+          columns: [
+            [{
+              text: experience.jobTitle,
+              style: 'jobTitle'
+            },
+            {
+              text: experience.employer,
+            },
+            {
+              text: experience.jobDescription,
+            }],
+            {
+              text: 'Experience : ' + experience.experience + ' Months',
+              alignment: 'right'
+            }
+          ]
+        }]
+      );
+    });
+
+    return {
+      table: {
+        widths: ['*'],
+        body: [
+          ...exs
+        ]
+      }
+    };
+  }
+
+  getEducationObject(educations: Education[]) {
+    return {
+      table: {
+        widths: ['*', '*', '*', '*'],
+        body: [
+          [{
+            text: 'Degree',
+            style: 'tableHeader'
+          },
+          {
+            text: 'College',
+            style: 'tableHeader'
+          },
+          {
+            text: 'Passing Year',
+            style: 'tableHeader'
+          },
+          {
+            text: 'Result',
+            style: 'tableHeader'
+          },
+          ],
+          ...educations.map(ed => {
+            return [ed.degree, ed.college, ed.passingYear, ed.percentage];
+          })
+        ]
+      }
+    };
+  }
+
+  getProfilePicObject() {
+    if (this.resume.profilePic) {
+      return {
+        image: this.resume.profilePic ,
+        width: 75,
+        alignment : 'right'
+      };
+    }
+    return null;
+  }
+
+  fileChanged(e) {
+    const file = e.target.files[0];
+    this.getBase64(file);
+  }
+
+  getBase64(file) {
+    const reader = new FileReader();
+    reader.readAsDataURL(file);
+    reader.onload = () => {
+      console.log(reader.result);
+      this.resume.profilePic = reader.result as string;
+    };
+    reader.onerror = (error) => {
+      console.log('Error: ', error);
+    };
+  }
+
+  addSkill() {
+    this.resume.skills.push(new Skill());
+  }
+
+}
---
+++ src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { AppComponent } from './app.component';
+import { ServiceWorkerModule } from '@angular/service-worker';
+import { environment } from '../environments/environment';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    ServiceWorkerModule.register('ngsw-worker.js', { enabled: environment.production })
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
